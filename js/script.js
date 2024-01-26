4572462d411c920f9689b532754d71dfe7ca58c5
g1ammi93 <giammariacarelli93@hotmail.it>
2024-01-26 16:11:39 +0000

    Search Area operation added

--- js/script.js
+++ js/script.js
@@ -5,12 +5,21 @@ const app = Vue.createApp({
         return {
             user,
             contacts,
-            activeId: 1
+            activeId: 1,
+            newMessageText: '',
+            searchText: ''
         }
     },
     computed: {
         currentContact() {
             return this.contacts.find(contact => contact.id === this.activeId);
+        },
+        // Aggiungo compted per filtrare i messaggi
+        filteredContacts() {
+            const searchTerm = this.searchText.toLowerCase().trim();
+            return this.contacts.filter(contact =>
+                contact.name.toLowerCase().includes(searchTerm)
+            );
         }
     },
     methods: {
@@ -19,6 +28,29 @@ const app = Vue.createApp({
         },
         getAvatarUrl({ avatar }) {
             return `img/avatar${avatar}.jpg`;
+        },
+        sendMessage() {
+            if (!this.newMessageText) return;
+            // Messaggio che invio pushato nel box
+            const newMessage = {
+                id: new Date().getTime(),
+                date: new Date().toLocaleDateString(),
+                status: 'sent',
+                text: this.newMessageText,
+            }
+            this.currentContact.messages.push(newMessage)
+            this.newMessageText = ''
+
+            // Aggiungo risposta dopo tot tempo
+            setTimeout(() => {
+                const newMessage = {
+                    id: new Date().getTime(),
+                    date: new Date().toLocaleDateString(),
+                    status: 'received',
+                    text: 'ok',
+                }
+                this.currentContact.messages.push(newMessage);
+            }, 1000);
         }
     }
 });
